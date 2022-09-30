29c75a855f36668702e8f9fb17308537525f776f
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2022-09-29 22:55:45 -0300

    cadastro

--- TCC/TCC/software/src/javascript.js
+++ TCC/TCC/software/src/javascript.js
@@ -3,8 +3,15 @@ function enter(){
     window.location.href = "intranet.php";    
 }
 function cadastro(){
-    alert("Teste cadastrar")
+    window.location.href = "cadastroUser.php";
 }
 function login(){
     window.location.href = "login.php";
 }
+function cancelar(){
+    alert("teste");
+    window.location.href = "login.php";
+}
+$('.celular').mask('(00)00000-0000');
+$('.rg').mask('11.111.111-1');
+$('.cpf').mask('000.000.000-00');
