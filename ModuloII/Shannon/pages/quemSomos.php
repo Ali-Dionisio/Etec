6f146ae799020bde4bc004c9dcdc1ec6fea47135
ericsanderson <106767210+ericsandersonc@users.noreply.github.com>
2022-11-06 00:48:42 -0300

    PaginaQuemSomosAdicionada
    Alteracao feita por Eric Sanderson em 06/10/2022

---
+++ ModuloII/Shannon/pages/quemSomos.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="../css/quemSomos.css">
+    <title>Quem Somos</title>
+</head>
+<body>
+    <div class="menu">
+        <h1>ESPAÇO PARA MENU</h1>
+    </div>
+
+    <div class="logoQS">
+            <div class="leadership">
+                <h1>
+                    Líderes notáveis com soluções notáveis
+                </h1>
+            </div>
+            <div class="imgLogoShannon2">
+                <img src="/images/ShannonLogo2.png" alt="" width="300" height="300">
+                </div>
+    </div>
+    
+    <div class="sobre">
+        <h4>
+            Somos um grupo de 4 amigos que acreditam
+            no progresso, ao aplicar a inteligência, 
+            o raciocínio e a ciência para encontrar 
+            soluções para condôminios e condominos.
+        </h4>
+    </div>
+
+    <div class="alisson">
+        <div class="imgAlisson">
+                <img src="/images/exMiniNoney.png" alt="">
+        </div>
+        
+            <div class="nomeAlisson">
+            <h2>Alisson Almeida</h2>
+                
+                <div class="cargoAlisson">
+                <b>Aluno 1</b>
+                </div>
+
+                    <div class="sobreAlisson">
+                        <p>O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online,
+                            você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave
+                            para pesquisar online o vídeo mais adequado ao seu documento. Para dar ao documento uma aparência profissional,
+                            o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si.
+                            Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes.
+                            Clique em Inserir e escolha os elementos desejados nas diferentes galerias. Temas e estilos também ajudam a manter
+                            seu documento coordenado.</p>
+                    </div>
+            </div>
+    </div>
+
+    <div class="eric">
+        <div class="imgEric">
+                <img src="/images/exMiniNoney.png" alt="">
+        </div>
+        
+            <div class="nomeEric">
+            <h2>Eric Sanderson</h2>
+                
+                <div class="cargoEric">
+                <b>Aluno 4</b>
+                </div>
+
+                    <div class="sobreEric">
+                        <p>O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online,
+                            você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave
+                            para pesquisar online o vídeo mais adequado ao seu documento. Para dar ao documento uma aparência profissional,
+                            o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si.
+                            Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes.
+                            Clique em Inserir e escolha os elementos desejados nas diferentes galerias. Temas e estilos também ajudam a manter
+                            seu documento coordenado.</p>
+                    </div>
+            </div>
+    </div>
+
+    <div class="gabriel">
+        <div class="imgGabriel">
+                <img src="/images/exMiniNoney.png" alt="">
+        </div>
+        
+            <div class="nomeGabriel">
+            <h2>Gabriel Sena</h2>
+                
+                <div class="cargoGabriel">
+                <b>Aluno 3</b>
+                </div>
+
+                    <div class="sobreGabriel">
+                        <p>O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online,
+                            você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave
+                            para pesquisar online o vídeo mais adequado ao seu documento. Para dar ao documento uma aparência profissional,
+                            o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si.
+                            Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes.
+                            Clique em Inserir e escolha os elementos desejados nas diferentes galerias. Temas e estilos também ajudam a manter
+                            seu documento coordenado.</p>
+                    </div>
+            </div>
+    </div>
+
+    <div class="joao">
+        <div class="imgJoao">
+                <img src="/images/exMiniNoney.png" alt="">
+        </div>
+        
+            <div class="nomeJoao">
+            <h2>João Henrique</h2>
+                
+                <div class="cargoJoao">
+                <b>Aluno 2</b>
+                </div>
+
+                    <div class="sobreJoao">
+                        <p>O vídeo fornece uma maneira poderosa de ajudá-lo a provar seu argumento. Ao clicar em Vídeo Online,
+                            você pode colar o código de inserção do vídeo que deseja adicionar. Você também pode digitar uma palavra-chave
+                            para pesquisar online o vídeo mais adequado ao seu documento. Para dar ao documento uma aparência profissional,
+                            o Word fornece designs de cabeçalho, rodapé, folha de rosto e caixa de texto que se complementam entre si.
+                            Por exemplo, você pode adicionar uma folha de rosto, um cabeçalho e uma barra lateral correspondentes.
+                            Clique em Inserir e escolha os elementos desejados nas diferentes galerias. Temas e estilos também ajudam a manter
+                            seu documento coordenado.</p>
+                    </div>
+            </div>
+    </div>
+
+    <div class="contato">
+        <h1>ENTRE EM CONTATO</h1>
+    </div>
+</body>
+</html>
