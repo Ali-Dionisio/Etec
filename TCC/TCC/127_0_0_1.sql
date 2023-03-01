3701eebcadb59b5a706bd5897fb45344a274e2c0
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2023-02-28 22:31:14 -0300

    Criado tabelas auxiliares cor e funcao
    Tabelas não necessitam de relacionamento com outras tabelas

--- TCC/TCC/127_0_0_1.sql
+++ TCC/TCC/127_0_0_1.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 16-Jan-2023 às 08:17
--- Versão do servidor: 5.7.36
+-- Tempo de geração: 01-Mar-2023 às 01:30
+-- Versão do servidor: 8.0.27
 -- versão do PHP: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -27,35 +26,88 @@ USE `bd_condmind`;
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tb_cor`
+--
+
+DROP TABLE IF EXISTS `tb_cor`;
+CREATE TABLE IF NOT EXISTS `tb_cor` (
+  `cod_cor` int NOT NULL AUTO_INCREMENT,
+  `descricao_cor` varchar(24) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`cod_cor`),
+  KEY `cod_cor` (`cod_cor`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `tb_cor`
+--
+
+INSERT INTO `tb_cor` (`cod_cor`, `descricao_cor`) VALUES
+(2, 'Preto'),
+(3, 'Prata'),
+(4, 'Vermelho'),
+(5, 'Azul'),
+(6, 'Amarelo'),
+(7, 'Rosa'),
+(8, 'Ouro'),
+(9, 'Branco');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_funcao`
+--
+
+DROP TABLE IF EXISTS `tb_funcao`;
+CREATE TABLE IF NOT EXISTS `tb_funcao` (
+  `cod_funcao` int NOT NULL AUTO_INCREMENT,
+  `descricao_funcao` varchar(24) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`cod_funcao`),
+  KEY `cod_funcao` (`cod_funcao`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `tb_funcao`
+--
+
+INSERT INTO `tb_funcao` (`cod_funcao`, `descricao_funcao`) VALUES
+(1, 'Morador'),
+(2, 'Porteiro'),
+(3, 'Administrador'),
+(4, 'Morador_Sindico');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tb_morador`
 --
 
 DROP TABLE IF EXISTS `tb_morador`;
 CREATE TABLE IF NOT EXISTS `tb_morador` (
-  `cod_morador` int(11) NOT NULL AUTO_INCREMENT,
-  `primeiro_nome` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `nome_completo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `cpf` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `rg` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
-  `dtnascimento` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
-  `celular` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
-  `estadocivil` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `bloco` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
-  `numero_apartamento` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
-  `foto` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `funcao` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `cod_morador` int NOT NULL AUTO_INCREMENT,
+  `primeiro_nome` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nome_completo` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `rg` varchar(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `dtnascimento` varchar(12) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `celular` varchar(13) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `estadocivil` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `bloco` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `numero_apartamento` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `foto` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cod_funcao` int NOT NULL,
   PRIMARY KEY (`cod_morador`),
-  UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `cpf` (`cpf`),
+  KEY `cod_funcao` (`cod_funcao`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `tb_morador`
 --
 
-INSERT INTO `tb_morador` (`cod_morador`, `primeiro_nome`, `nome_completo`, `email`, `cpf`, `rg`, `dtnascimento`, `celular`, `estadocivil`, `bloco`, `numero_apartamento`, `foto`, `funcao`) VALUES
-(1, 'alisson', 'alisson almeida', 'alisson.a-work@outlook.com', '44291300867', '111', '1999-01-20', '992325271', 'Casado(a)', '6', '1', '../imgs/1ac5fbea12c9dc646435a69daa443270.jpg', 'Administrador'),
-(22, 'Eric', 'Eric Sanderson', '', '1234', '11111', '2022-12-14', '11999995555', 'Solteiro(a)', '3', '12', '../imgs/14c837e9c88f9cf725e98282206992e5.jpg', 'Morador');
+INSERT INTO `tb_morador` (`cod_morador`, `primeiro_nome`, `nome_completo`, `email`, `cpf`, `rg`, `dtnascimento`, `celular`, `estadocivil`, `bloco`, `numero_apartamento`, `foto`, `cod_funcao`) VALUES
+(1, 'alisson', 'alisson almeida', 'alisson.a-work@outlook.com', '44291300867', '111', '1999-01-20', '992325271', 'Casado(a)', '6', '1', '../imgs/1ac5fbea12c9dc646435a69daa443270.jpg', 0),
+(23, 'Eric', 'Eric Sanderson', 'eric@etec.com', '49098843867', '', '2022-12-14', '', 'Solteiro(a)', '3', '12', '../imgs/14c837e9c88f9cf725e98282206992e5.jpg', 0);
 
 --
 -- Acionadores `tb_morador`
@@ -88,13 +140,13 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `tb_salao`;
 CREATE TABLE IF NOT EXISTS `tb_salao` (
-  `cod_salao` int(11) NOT NULL,
+  `cod_salao` int NOT NULL,
   `data_reserva` date NOT NULL,
   `hora_inicio` time NOT NULL,
   `hora_fim` time NOT NULL,
-  `cod_morador` int(11) NOT NULL,
+  `cod_morador` int NOT NULL,
   UNIQUE KEY `cod_morador` (`cod_morador`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -104,22 +156,21 @@ CREATE TABLE IF NOT EXISTS `tb_salao` (
 
 DROP TABLE IF EXISTS `tb_usuarios`;
 CREATE TABLE IF NOT EXISTS `tb_usuarios` (
-  `cod_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `primeiro_nome` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `senha` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `funcao` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `cod_usuario` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `primeiro_nome` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`cod_usuario`),
   UNIQUE KEY `usuario` (`usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `tb_usuarios`
 --
 
-INSERT INTO `tb_usuarios` (`cod_usuario`, `usuario`, `primeiro_nome`, `senha`, `funcao`) VALUES
-(4, '44291300867', 'Alisson', '21232f297a57a5a743894a0e4a801fc3', 'Administrador'),
-(15, '1234', 'Eric', '81dc9bdb52d04dc20036dbd8313ed055', 'Morador');
+INSERT INTO `tb_usuarios` (`cod_usuario`, `usuario`, `primeiro_nome`, `senha`) VALUES
+(4, '44291300867', 'Alisson', '21232f297a57a5a743894a0e4a801fc3'),
+(16, '49098843867', 'Eric', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -129,15 +180,15 @@ INSERT INTO `tb_usuarios` (`cod_usuario`, `usuario`, `primeiro_nome`, `senha`, `
 
 DROP TABLE IF EXISTS `tb_vaga_garagem`;
 CREATE TABLE IF NOT EXISTS `tb_vaga_garagem` (
-  `cod_vaga` int(11) NOT NULL AUTO_INCREMENT,
-  `num_vaga` int(11) NOT NULL,
-  `tipo_vaga` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `cod_vaga` int NOT NULL AUTO_INCREMENT,
+  `num_vaga` int NOT NULL,
+  `tipo_vaga` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `ocupada` tinyint(1) NOT NULL,
-  `placa_veiculo` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
+  `placa_veiculo` varchar(7) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`cod_vaga`),
   UNIQUE KEY `placa_veiculo_2` (`placa_veiculo`),
   KEY `placa_veiculo` (`placa_veiculo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -147,14 +198,15 @@ CREATE TABLE IF NOT EXISTS `tb_vaga_garagem` (
 
 DROP TABLE IF EXISTS `tb_veiculo_morador`;
 CREATE TABLE IF NOT EXISTS `tb_veiculo_morador` (
-  `placa_veiculo` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
-  `modelo` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `placa_veiculo` varchar(7) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `modelo` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `ano` date NOT NULL,
-  `cor` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `cod_morador` int(11) NOT NULL,
+  `cod_cor` int NOT NULL,
+  `cod_morador` int NOT NULL,
   PRIMARY KEY (`placa_veiculo`),
-  UNIQUE KEY `cod_morador` (`cod_morador`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `cod_morador` (`cod_morador`),
+  KEY `cod_cor` (`cod_cor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
 -- Restrições para despejos de tabelas
