a0da76cda8983325c0ce179614288c0c14cc9d32
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2023-03-15 19:17:05 -0300

    padronizando conforme miro

--- TCC/TCC/software1/pages/intranet/usuarios.php
+++ TCC/TCC/software1/pages/intranet/usuarios.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="../estilo/moradores.css">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <title>Listar Usuários</title>
     <script>
         function pesquisa(texto){
@@ -21,7 +24,7 @@
     </script>
 </head>
 <body>
-<?php include('sidebar.php'); ?>    
+<?php include('../intranet/barraSuperiorInt.php'); ?>    
 
     <div class="pesquisar">
         <p>Pesquisar <input type="text" onKeyup="pesquisa(this.value)"></p>
@@ -41,24 +44,28 @@
     require('../acoes/connect.php');
     $usuarios = mysqli_query($con, "Select * from `tb_usuarios`");
     while($usuario = mysqli_fetch_array($usuarios)){
+        echo "<div class=box2>";
+        
         echo "<div class=box1>";
         echo "<div class=box>";
-        echo "<p> Cod. Usuário: $usuario[cod_usuario]</p>";
-        echo "<p> Primeiro Nome: $usuario[primeiro_nome]</p>";
+        echo "<p> Cod. Usuário:<b> $usuario[cod_usuario]</b></p>";
+        echo "<p> Primeiro Nome:<b> $usuario[primeiro_nome]</b></p>";
         echo "</div>";
         echo "<div class=box>";
-        echo "<p> Usuário: $usuario[usuario]</p>";
-        echo "<p> Função: $usuario[funcao]</p>";
+        echo "<p> Usuário:<b> $usuario[usuario]</b></p>";
+        echo "<p> Função:<b> $usuario[funcao]</b></p>";
         echo "</div>";
         echo "<div class=box>";
-        echo "<p> Senha: $usuario[senha]</p>";
+        echo "<p> Senha:<b> $usuario[senha]</b></p>";
         echo "</div>";
         echo "<div class=alterarExcluir>";
-        echo "<p> <a href =alterarUsuario.php?cod=$usuario[cod_usuario]>Alterar</a></p>";
-        echo "<p> <a href =javascript:confirmar($usuario[cod_usuario])>Excluir</a></p>";
+        echo "<p class=alterar> <a href =alterarUsuario.php?cod=$usuario[cod_usuario]>Alterar</a></p>";
+        echo "<p class=excluir> <a href =javascript:confirmar($usuario[cod_usuario])>Excluir</a></p>";
         echo "</div>";
         echo "</div>";
         echo "<div class=risco> </div>";
+        echo "</div>";
+
 
 
     }
