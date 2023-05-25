377c9fe5102de4a8db1cde5fdd78977ade9e6a2d
Alisson <alisson@rosset.com.br>
2023-05-25 15:44:21 -0300

    linhas de comando para acessar o bd hostinger

--- TCC/TCC/software1/pages/acoes/connect.php
+++ TCC/TCC/software1/pages/acoes/connect.php
@@ -3,4 +3,13 @@
         echo "Erro ao acessar a Base de Dados";
     };
     mysqli_query($con,"SET NAMES utf8");
+
+    /*
+<?php
+    if ( !@$con = mysqli_connect('localhost','u525967977_root','TccEtec2023','u525967977_bd_condmind')){
+        echo "Erro ao acessar a Base de Dados";
+    };
+    mysqli_query($con,"SET NAMES utf8");
+?>
+    */
 ?>
